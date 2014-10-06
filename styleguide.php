bfb353215cfb645b06052891fc1e810b921b0320
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2014-10-06 14:25:57 +0100

    commit

--- styleguide.php
+++ styleguide.php
@@ -27,6 +27,7 @@
 
     <blockquote>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p>
+        <cite>- Luke Whitehouse, <?php echo date('Y'); ?></cite>
     </blockquote>
 
 <pre><code>
