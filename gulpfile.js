202936d69216b4aeb53d9651de40531dd2cd9b18
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2015-03-13 17:15:36 +0000

    added gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+var paths = {
+    assets: 'assets/',
+    css: 'assets/css/',
+    scss: 'assets/scss/',
+    js: 'assets/js/',
+    img: 'assets/img/',
+    bower: 'components/'
+}
+
+gulp.task('sass', function () {
+    gulp.src([paths.scss] + 'styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest([paths.css] + ''));
+});
 
 gulp.task('default', function() {
-    // Default Tasks
+    
 });
