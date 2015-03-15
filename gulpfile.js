fe8598891769ff86f011af3d87d99d247d12e0ef
lukewhitehouse <luke@lukewhitehouse.co.uk>
2015-03-15 01:02:11 +0000

    add autoprefixer to 'sass' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+// Require Node Packages
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
 
+// Create asset variables
 var paths = {
     assets: 'assets/',
     css: 'assets/css/',
@@ -15,6 +18,10 @@ gulp.task('sass', function () {
     gulp.src( [paths.scss] + '*.scss' )
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+            cascade: false
+        }))
         .pipe(sourcemaps.write( './maps' ))
         .pipe(gulp.dest( [paths.css] + '' ));
 });
